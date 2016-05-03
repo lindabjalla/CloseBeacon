1a0952b4c14a02f9e87a43c68cd96f0bfaa31b49
HArbietoCortez <arbietohaydee@hotmail.com>
2016-05-03 13:42:11 +0200

    Lagt till Bluetooth permition i Manifest...

---
+++ app/src/main/java/grouprich/se/closebeacon/controller/RSAEncrypt.java
@@ -0,0 +1,27 @@
+package grouprich.se.closebeacon.controller;
+
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+
+public class RSAEncrypt {
+
+    private final static String RSA = "RSA";
+    public static PublicKey publicKey;
+    public static PrivateKey privateKey;
+
+    public static void generateKey() throws Exception{
+        KeyPairGenerator generator = KeyPairGenerator.getInstance(RSA);
+
+        generator.initialize(512, new SecureRandom());
+
+        KeyPair keyPair = generator.generateKeyPair();
+        publicKey = keyPair.getPublic();
+        privateKey = keyPair.getPrivate();
+    }
+
+    
+}
