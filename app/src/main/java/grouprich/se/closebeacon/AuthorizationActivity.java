b652c09e3e5d1c3bbc5716be8cff712d1b9d8ce9
HArbietoCortez <arbietohaydee@hotmail.com>
2016-05-03 13:42:31 +0200

    RSAEncrypt

---
+++ app/src/main/java/grouprich/se/closebeacon/AuthorizationActivity.java
@@ -0,0 +1,19 @@
+package grouprich.se.closebeacon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class AuthorizationActivity extends AppCompatActivity {
+
+    private Button buttonAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authorization);
+
+        buttonAuth = (Button)findViewById(R.id.button_auth);
+        buttonAuth.setOnClickListener();
+    }
+}
