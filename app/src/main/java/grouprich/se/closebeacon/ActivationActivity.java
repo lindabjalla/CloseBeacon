72db37c20b24fb1ced9633ff3e29824f9d532efb
lindabjalla <lindabjalla@gmail.com>
2016-05-05 00:57:46 +0200

    Lägger till onClick i activity_activation.xml

--- app/src/main/java/grouprich/se/closebeacon/ActivationActivity.java
+++ app/src/main/java/grouprich/se/closebeacon/ActivationActivity.java
@@ -9,7 +9,5 @@ public class ActivationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activation);
-
-
     }
 }
